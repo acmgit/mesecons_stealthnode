4cd4a7fe5ac3e804eb54bc855e0ec847096d290e
OgelGames <olliverdc28@gmail.com>
2020-10-29 20:09:30 +1100

    Fix bugs and cleanup mod

--- register.lua
+++ register.lua
@@ -1,88 +1,105 @@
+
 --[[
-       ********************************************
-       **                                        **
-       **           Stealthnode-Register         **
-       **                                        **
-       **        Helpmod for Stealthnode to      **
-       **      easy Register new Stealthnodes    **
-       **                                        **
-       ********************************************
+    ********************************************
+    ***         Register Stealthnodes        ***
+    ********************************************
+
+    Registers a new Stealthnode with the tile of the given node.
+
+    stealthnode.register_stealthnode(modname, node)
+
+    modname = String. Name of the mod without ":", for example "default".
+    node = String. Name of the registered node, for example "sandstone".
+
 ]]--
 
-local snodes = {
+local function copy_table(t)
+	if type(t) ~= "table" then return end
+	local t2 = {}
+	for k,v in pairs(t) do
+		t2[k] = v
+	end
+	return t2
+end
 
-		{"default", "dirt"},
-		{"default", "dry_dirt"},
-		{"default", "permafrost"},
-		
-		{"default", "sand"},
-		{"default", "silver_sand"},
-		{"default", "desert_sand"},
-		
-		{"default", "gravel"},
-		{"default", "clay"},
-		{"default", "snowblock"},
-		
-		{"default", "ice"},
-		{"default", "cave_ice"},
-		
-                {"default", "cobble"},
-                {"default", "stone"},
-                {"default", "stonebrick"},
-                {"default", "stone_block"},
-		{"default", "mossycobble"},
-		
-                --{"default", "glass"},
-                --{"default", "obsidian_glass"},
+function stealthnode.register_stealthnode(modname, node)
 
-                {"default", "desert_cobble"},
-                {"default", "desert_stone"},
-                {"default", "desert_stonebrick"},
-                {"default", "desert_stone_block"},
+	local node_name = modname .. ":" .. node
 
-                {"default", "sandstone"},
-                {"default", "sandstonebrick"},
-                {"default", "sandstone_block"},
+	local nodedef = minetest.registered_nodes[node_name]
 
-                {"default", "desert_sandstone"},
-                {"default", "desert_sandstone_brick"},
-                {"default", "desert_sandstone_block"},
+	if not nodedef then
+		local message = "[MOD] " .. minetest.get_current_modname() .. ": "
+			.. node_name .. " not found to register a stealthnode."
+		print(message)
+		minetest.log("warning", message)
+		return
+	end
 
-                {"default", "silver_sandstone"},
-                {"default", "silver_sandstone_brick"},
-                {"default", "silver_sandstone_block"},
+	local stealthnode_name = "mesecons_stealthnode:" .. modname .. "_" .. node
 
-                {"default", "obsidian"},
-                {"default", "obsidianbrick"},
-                {"default", "obsidian_block"},
-		
-		{"default", "coral_skeleton"},
-		
-                {"default", "wood"},
-                --{"default", "tree"},
+	local node_groups = copy_table(nodedef.groups) or {}
+	node_groups.mesecons_stealthnode = 1
 
-                {"default", "junglewood"},
-                --{"default", "jungletree"},
+	minetest.register_node(":" .. stealthnode_name, {
+		description = "Stealthnode " .. nodedef.description,
+		drawtype = nodedef.drawtype,
+		tiles = nodedef.tiles,
+		use_texture_alpha = nodedef.use_texture_alpha,
+		paramtype = nodedef.paramtype,
+		paramtype2 = nodedef.paramtype2,
+		is_ground_content = false,
+		sunlight_propagates = nodedef.sunlight_propagates,
+		node_box = nodedef.node_box,
+		mesh = nodedef.mesh,
+		selection_box = nodedef.selection_box,
+		collision_box = nodedef.collision_box,
+		sounds = nodedef.sounds,
+		groups = node_groups,
+		inventory_image = nodedef.inventory_image,
+		mesecons = {
+			conductor = {
+				state = mesecon.state.off,
+				rules = mesecon.rules.alldirs,
+				onstate = stealthnode_name .. "_active",
+			}
+		},
+		on_blast = mesecon.on_blastnode,
+	})
 
-                {"default", "aspen_wood"},
-                --{"default", "aspen_tree"},
+	minetest.register_node(":" .. stealthnode_name .. "_active", {
+		drawtype = "airlike",
+		paramtype = "light",
+		is_ground_content = false,
+		sunlight_propagates = true,
+		pointable = false,
+		walkable = false,
+		diggable = false,
+		drop = stealthnode_name,
+		groups = {mesecons_stealthnode_active = 1, not_in_creative_inventory = 1},
+		mesecons = {
+			conductor = {
+				state = mesecon.state.on,
+				rules = mesecon.rules.alldirs,
+				offstate = stealthnode_name,
+			}
+		},
+		on_construct = function(pos)
+			-- remove shadow
+			local shadowpos = vector.add(pos, vector.new(0, 1, 0))
+			if minetest.get_node(shadowpos).name == "air" then
+				minetest.dig_node(shadowpos)
+			end
+		end,
+		on_blast = mesecon.on_blastnode,
+	})
 
-                {"default", "pine_wood"},
-                --{"default", "pine_tree"},
-
-                {"default", "acacia_wood"},
-                --{"default", "acacia_tree"},
-
-                {"moreores", "mithril_block"},
-
-            }
-
-local register = stealthnode.register_stealthnode
-for i,value in pairs(snodes) do
-    if(minetest.registered_nodes[value[1]..":"..value[2]]) then
-	register(value[1], value[2])
-    else
-        minetest.log("info","[MOD]stealthnode:Node ".. value[1]..":"..value[2].." not found to register a Stealthnode.")
-    end -- if(mintest.registered_nodes
-
-end -- for i,value
+	minetest.register_craft({
+		output = stealthnode_name .. " 4",
+		recipe = {
+			{"default:tin_ingot", node_name, "default:tin_ingot"},
+			{node_name, "group:mesecon_conductor_craftable", node_name},
+			{"default:tin_ingot", node_name, "default:tin_ingot"},
+		}
+	})
+end
